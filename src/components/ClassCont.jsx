3cb85120157e794eded187e2fa9860c85c162308
fkdldhs8484 <fkdldhs8484@gmail.com>
2022-11-25 11:09:04 +0900

    1

--- src/components/ClassCont.jsx
+++ src/components/ClassCont.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import star from '../assets/img/star.png'
+
 const ClassCont = () => {
   return (
     <section className="class__wrap">
@@ -73,19 +75,19 @@ const ClassCont = () => {
               <div>
                 <img src="/assets/mando1.png" alt="만두이미지1" />
                 <div className="title">비엔나 만두 강정</div>
-                <img src="/assets/star.png" alt="별" />
+                <img src={star} alt="별" />
               </div>
               <div>
                 <img src="/assets/mando2.png" alt="만두이미지1" />
                 <div className="title">
                   만두강정 소스까지 다 만들기 간단간식 맥주 안주로 굿
                 </div>
-                <img src="/assets/star.png" alt="별" />
+                <img src={star} alt="별" />
               </div>
               <div>
                 <img src="/assets/mando3.png" alt="만두이미지1" />
                 <div className="title">마늘종 오일 파스타 만들기</div>
-                <img src="/assets/star.png" alt="별" />
+                <img src={star} alt="별" />
               </div>
             </div>
             <div className="man-b">
@@ -94,19 +96,19 @@ const ClassCont = () => {
                 <div className="title">
                   간단하지만 중독성있게 맛있는 간장파스타
                 </div>
-                <img src="/assets/star.png" alt="별" />
+                <img src={star} alt="별" />
               </div>
               <div>
                 <img src="/assets/mando5.png" alt="만두이미지1" />
                 <div className="title">차돌박이 파스타</div>
-                <img src="/assets/star.png" alt="별" />
+                <img src={star} alt="별" />
               </div>
               <div>
                 <img src="/assets/mando6.png" alt="만두이미지1" />
                 <div className="title">
                   깐풍만두 전분가루 없이도 맛있는 소스만들기
                 </div>
-                <img src="/assets/star.png" alt="별" />
+                <img src={star} alt="별" />
               </div>
             </div>
           </div>
