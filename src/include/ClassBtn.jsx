5398d78e5b4b46876848e8be04e907d3c3cf2af6
fkdldhs8484 <fkdldhs8484@gmail.com>
2022-11-28 18:13:03 +0900

    11.28

---
+++ src/include/ClassBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ClassBtn = ({ displayName }) => {
+  const display1 = () => {
+    displayName('Crazy About Cookies')
+  }
+
+  return <div>ClassBtn</div>
+}
+
+export default ClassBtn
