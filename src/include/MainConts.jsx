56ef45974eae258d216a7b32b8efd64712786e3c
fkdldhs8484 <fkdldhs8484@gmail.com>
2022-12-01 09:48:31 +0900

    121

--- src/include/MainConts.jsx
+++ src/include/MainConts.jsx
@@ -6,6 +6,35 @@ const MainConts = () => {
       <div className="slider__inner">
         <div className="slider__box">
           <h2>Today's Recommended Recipe~</h2>
+          <div className="slider__recipes">
+            <ul>
+              <li>
+                <a href="/">
+                  <img src="" alt="이미지" />
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  <img src="" alt="이미지" />
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  <img src="" alt="이미지" />
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  <img src="" alt="이미지" />
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  <img src="" alt="이미지" />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
